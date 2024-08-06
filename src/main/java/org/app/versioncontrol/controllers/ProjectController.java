9a1245e4bca6dc0692122ccf8d936b814730db4f
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-08-07 02:26:32 +0300

    Change url

--- src/main/java/org/app/versioncontrol/controllers/ProjectController.java
+++ src/main/java/org/app/versioncontrol/controllers/ProjectController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/vercontr/user")
 public class ProjectController {
 
     @Autowired
